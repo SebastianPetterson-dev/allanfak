7c315919a982b9f4f2ec816a70cbe6c12f480f31
DevReframe <dev@reframe.nu>
2024-10-23 17:34:10 +0200

    hello

--- src/app/api/generate-pdf/route.ts
+++ src/app/api/generate-pdf/route.ts
@@ -9,17 +9,10 @@ export async function POST(request: NextRequest) {
     customerName,
     customerAddress,
     customerEmail,
-    senderName,
-    senderAddress,
-    senderEmail,
     items,
     subtotal,
     tax,
     total,
-    dueDateFinal,
-    payFinal,
-    accountNumber,
-    regNumber,
   } = await request.json();
 
   const pdfDoc = await PDFDocument.create();
